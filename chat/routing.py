c98f43e8ac6ca3dd2a9d2d90e75c4c2bae84a274
acharyabidhan <acharyaumesh2005@gmail.com>
2022-08-01 09:38:16 +0545

    sc

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_relpatterns = [
-    re_path('chat-server/', consumers.ChatConsumer.as_asgi())
+    re_path('ws/socket-server/', consumers.ChatConsumer.as_asgi())
 ]
