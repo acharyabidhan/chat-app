05ebeb34e47415e01d0795f24d9ca8451acd1d21
acharyabidhan <acharyaumesh2005@gmail.com>
2022-08-04 10:43:42 +0545

    revolutionary change

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,34 +1,15 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
-
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.group_name = 'test'
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
+        self.group_name = 'allInOne'
+        async_to_sync(self.channel_layer.group_add)(self.group_name,self.channel_name)
         self.accept()
-        self.send(text_data=json.dumps({
-            'type':"connection established",
-            'msg':"hello world"
-        }))
-    
+        self.send(text_data=json.dumps({"status":"connected"}))
     def receive(self, text_data=None, bytes_data=None):
         txt_data_msg = json.loads(text_data)
-        msg = txt_data_msg['msg']
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name,
-            {
-                'type':'chat_message',
-                'msg':msg
-            }
-        )
-    
-    def chat_message(self, e):
-        msg = e['msg']
-        self.send(text_data=json.dumps({
-            'type':'chat',
-            'msg':msg
-        }))
+        async_to_sync(self.channel_layer.group_send)(self.group_name,{'type':'broadcast_all','data':txt_data_msg})
+    def broadcast_all(self, e):
+        msg = e['data']
+        self.send(text_data=json.dumps(msg))
